49922d2a17afd836d34c4a8600d9e4ffdbca23eb
Debdutta Paul <dbdttpl@gmail.com>
2023-10-14 20:04:36 +0530

    Update freelance_writing.py

--- freelance_writing.py
+++ freelance_writing.py
@@ -25,7 +25,7 @@ col7  =  data['col7'].data
 full_data       =  col1[1:]
 remuneration    =  col2[1:]
 number_of_words =  col3[1:]
-average_pay     =  col4[1:]
+perword_pay     =  col4[1:]
 News_flag       =  col5[1:]
 English_flag    =  col6[1:]
 Indian_flag     =  col7[1:]
@@ -36,7 +36,7 @@ print ( '\nNumber: ', len(full_data), '\n' )
 
 
 ## Tabulates the column with shorter, identifiable header names.
-table = Table( [ remuneration, number_of_words, average_pay, News_flag, English_flag, Indian_flag ],
+table = Table( [ remuneration, number_of_words, perword_pay, News_flag, English_flag, Indian_flag ],
 names =  ['Remuneration', 'Number of words', 'Average (INR)', 'News', 'English', 'Indian' ] )
 
 ### Prints the table to check.
@@ -92,29 +92,29 @@ def select_nonblanks( array1, array2 ):
 
 
 # ~ ## Removes blank data from the pay_per_word array.
-# ~ average_pay = np.round( remove_blanks( average_pay ), 1)
+# ~ perword_pay = remove_blanks( perword_pay )
 
 # ~ ## Tests authenticity, checks outliers, and prints some required numbers.
 # ~ print ('\n')
-# ~ print ( len(average_pay) )
-# ~ #print ( average_pay )
-# ~ print ( np.sort(average_pay), '\n' )
-# ~ #print ( (average_pay < 0).any() )
-# ~ print ( 'Min   : ', np.min(   average_pay) )
-# ~ print ( 'Max   : ', np.max(   average_pay) )
-# ~ print ( 'Mean  : ', np.mean(  average_pay) )
-# ~ print ( 'Median: ', np.median(average_pay) )
+# ~ print ( len(perword_pay) )
+# ~ #print ( perword_pay )
+# ~ print ( np.sort( np.round(perword_pay, 2) ), '\n' )
+# ~ #print ( (perword_pay < 0).any() )
+# ~ print ( 'Min   : ', np.min(   perword_pay) )
+# ~ print ( 'Max   : ', np.max(   perword_pay) )
+# ~ print ( 'Mean  : ', np.mean(  perword_pay) )
+# ~ print ( 'Median: ', np.median(perword_pay) )
 # ~ print ('\n')
 
 # ~ ## Plots histogram of the non-outliers.
-# ~ plt.hist( average_pay, range = (0, 13), rwidth = 0.7, color = 'r', alpha = 0.6, align = 'left' )
-# ~ x_ticks = np.arange(0, 14)
+# ~ plt.hist( perword_pay, range = (0, 13), rwidth = 0.7, color = 'r', alpha = 0.6, align = 'left' )
+# ~ x_ticks = np.arange(0, 13)
 # ~ y_ticks = np.arange(0, 13)
 # ~ plt.xticks( x_ticks, x_ticks )
 # ~ plt.yticks( y_ticks, y_ticks )
 # ~ plt.xlabel( "Freelance writers' pay per word [Indian Rupees]", labelpad=7 )
 # ~ plt.ylabel( "Number of instances" )
-# ~ plt.text( 8, 9, "Ignores outliers (INR):\n25.0, 56.2, 141.7." )
+# ~ plt.text( 8, 9, "Ignores outliers (INR):\n25.0, 56.3, 141.7." )
 # ~ plt.tight_layout()
 # ~ plt.savefig( './../plots/freelance_writerS_per_word--histogram.pdf' )
 # ~ plt.clf()
@@ -138,7 +138,7 @@ def select_nonblanks( array1, array2 ):
 # ~ y_ticks = np.arange(0, 29, 2)
 # ~ plt.xticks( x_ticks, (x_ticks/1000).astype(int) )
 # ~ plt.yticks( y_ticks, y_ticks )
-# ~ plt.xlabel( "Freelance writers' lump_sum remuneration [in 1000 Indian Rupees]", labelpad=7 )
+# ~ plt.xlabel( "Freelance writers' lump sum remuneration [in 1000 Indian Rupees]", labelpad=7 )
 # ~ plt.ylabel( "Number of instances" )
 # ~ plt.grid()
 # ~ plt.tight_layout()
@@ -155,33 +155,40 @@ def select_nonblanks( array1, array2 ):
 
 
 # ~ ## Selects the non-blank pay_per_word data and the corresponding Indian flags.
-# ~ X1, Indian_flag_avPay = select_nonblanks( average_pay, Indian_flag )
+# ~ X1, Indian_flag_pwPay = select_nonblanks( perword_pay, Indian_flag )
 
 # ~ ## Segregates the pay_per_word data into Indian and foreign clients.
-# ~ inds1 = np.where( Indian_flag_avPay == 'Yes' )[0]
-# ~ avPay_Indian  = X1[inds1]
-# ~ avPay_foreign = np.delete( X1, inds1 )
+# ~ inds1 = np.where( Indian_flag_pwPay == 'Yes' )[0]
+# ~ pwPay_Indian  = X1[inds1]
+# ~ pwPay_foreign = np.delete( X1, inds1 )
 
 
 # ~ ## Selects the non-blank pay_per_word data and the corresponding English flags.
-# ~ X2, English_flag_avPay = select_nonblanks( average_pay, English_flag )
+# ~ X2, English_flag_pwPay = select_nonblanks( perword_pay, English_flag )
 
 # ~ ## Segregates the pay_per_word data into English and non-English work.
-# ~ inds2 = np.where( English_flag_avPay == 'No' )[0]
-# ~ avPay_regional = X2[inds2]
-# ~ avPay_English  = np.delete( X2, inds2 )
+# ~ inds2 = np.where( English_flag_pwPay == 'No' )[0]
+# ~ pwPay_regional = X2[inds2]
+# ~ pwPay_English  = np.delete( X2, inds2 )
 
-## Checks authenticity and prints some required numbers.
+# ~ ## Checks authenticity and prints some required numbers.
 # ~ print ('\n')
-# ~ print ( len(avPay_Indian), '\n' )
+# ~ print ( len(pwPay_Indian), '\n' )
 # ~ print ( (X1 == X2).all(), '\n' )
-# ~ print ( len(avPay_English), '\n' )
+# ~ print ( len(pwPay_English), '\n' )
 # ~ print ( inds1, '\n')
 # ~ print ( inds2, '\n')
-# ~ print ( 'Median, Indian  : ', np.median( avPay_Indian ) )
-# ~ print ( 'Median, Foreign : ', np.median( avPay_foreign) )
+# ~ print ( 'Median, Indian  : ', np.median( pwPay_Indian ) )
+# ~ print ( 'Median, Foreign : ', np.median( pwPay_foreign) )
 # ~ print ( '\n' )
-
+# ~ print ( pwPay_foreign )
+# ~ print ( 'Number      of Indian clients paying <  5.0/word: ', len(np.where( pwPay_Indian <   5.0 )[0]) )
+# ~ print ( 'Perecentage of Indian clients paying <  5.0/word: ', 100 * len( np.where( pwPay_Indian <  5.0 )[0] ) / len(pwPay_Indian)  )
+# ~ print ( 'Number      of Indian clients paying < 10.0/word: ', len(np.where( pwPay_Indian <  10.0 )[0]) )
+# ~ print ( 'Perecentage of Indian clients paying < 10.0/word: ', 100 * len( np.where( pwPay_Indian < 10.0 )[0] ) / len(pwPay_Indian)  )
+# ~ print ( 'Number of non-English clients paying >  5.0/word: ', len( np.where( pwPay_regional >5.0 )[0] )  )
+# ~ print ( 'Highest paid Indian client paid how much        ? ', np.max(pwPay_Indian)  )
+# ~ print ( '\n' )
 
 ## Plots pay_per_word scatter plot with three different categories.
 # ~ plt.tick_params( axis='x', which='both', bottom=False, top=False, labelbottom=False )
@@ -189,9 +196,9 @@ def select_nonblanks( array1, array2 ):
 # ~ plt.ylim( -4, 150 )
 # ~ plt.yticks( y_ticks, y_ticks )
 # ~ plt.ylabel( "Freelance writers' pay per word [Indian Rupees]", labelpad=7 )
-# ~ plt.plot( avPay_foreign , 'o', color = 'dodgerblue', label = 'Foreign client (English)'   )
-# ~ plt.plot( avPay_Indian  , 'o', color = 'darkgreen' , label = 'Indian client: English'     )
-# ~ plt.plot( avPay_regional, 'o', color = 'r'         , label = 'Indian client: non-English' )
+# ~ plt.plot( pwPay_foreign , 'o', color = 'dodgerblue', label = 'Foreign client, English'    )
+# ~ plt.plot( pwPay_Indian  , 'o', color = 'darkgreen' , label = 'Indian client, English'     )
+# ~ plt.plot( pwPay_regional, 'o', color = 'r'         , label = 'Indian client, non-English' )
 # ~ plt.legend()
 # ~ plt.grid()
 # ~ plt.tight_layout()
@@ -200,19 +207,19 @@ def select_nonblanks( array1, array2 ):
 # ~ plt.clf()
 # ~ plt.close()
 
-# ~ ## Plots vertical plot of pay_per_word. Later discarded.
-# ~ #Indian_x_array  = 2 * np.ones(len(avPay_Indian ))
-# ~ #foreign_x_array = 3 * np.ones(len(avPay_foreign))
-# ~ #plt.xlim(1, 4)
-# ~ #plt.tick_params( axis='x', which='both', bottom=False, top=False, labelbottom=False)
-# ~ #plt.ylabel( "Freelance writers' pay per word [Indian Rupees]", labelpad=7 )
-# ~ #plt.plot( Indian_x_array , avPay_Indian , 'ko', label = 'Indian'     )
-# ~ #plt.plot( foreign_x_array, avPay_foreign, 'ro', label = 'non-Indian' )
-# ~ #plt.legend()
-# ~ #plt.tight_layout()
-# ~ #plt.savefig( './../plots/freelancer_writerS_per_word--vertical_plot.pdf' )
-# ~ #plt.clf()
-# ~ #plt.close()
+## Plots vertical plot of pay_per_word. Later discarded.
+#Indian_x_array  = 2 * np.ones(len(pwPay_Indian ))
+#foreign_x_array = 3 * np.ones(len(pwPay_foreign))
+#plt.xlim(1, 4)
+#plt.tick_params( axis='x', which='both', bottom=False, top=False, labelbottom=False)
+#plt.ylabel( "Freelance writers' pay per word [Indian Rupees]", labelpad=7 )
+#plt.plot( Indian_x_array , pwPay_Indian , 'ko', label = 'Indian'     )
+#plt.plot( foreign_x_array, pwPay_foreign, 'ro', label = 'non-Indian' )
+#plt.legend()
+#plt.tight_layout()
+#plt.savefig( './../plots/freelancer_writerS_per_word--vertical_plot.pdf' )
+#plt.clf()
+#plt.close()
 
 
 
@@ -236,7 +243,7 @@ def select_nonblanks( array1, array2 ):
 # ~ lumpsum_regional = X4[inds4]
 # ~ lumpsum_English  = np.delete( X4, inds4 )
 
-## Tests consistency check and prints required indices.
+# ~ ## Tests consistency check and prints required indices.
 # ~ print ('\n')
 # ~ print ( len(lumpsum_Indian), '\n' )
 # ~ print ( (X3 == X4).all(), '\n' )
@@ -245,15 +252,15 @@ def select_nonblanks( array1, array2 ):
 # ~ print ( inds4, '\n')
 # ~ print ( '\n' )
 
-## Plots lump_sum_pay scatter plot with three different categories.
+# ~ ## Plots lump_sum_pay scatter plot with three different categories.
 # ~ plt.tick_params( axis='x', which='both', bottom=False, top=False, labelbottom=False )
 # ~ y_ticks = np.arange( 0, 70, 5 )
 # ~ plt.ylim( -3, 65 )
 # ~ plt.yticks( y_ticks, y_ticks )
 # ~ plt.ylabel( "Freelance writers' lump sum remuneration [in 1000 Indian Rupees]", labelpad=7 )
-# ~ plt.plot( lumpsum_foreign *1e-3, 'o', color = 'dodgerblue', label = 'Foreign client (English)'   )
-# ~ plt.plot( lumpsum_Indian  *1e-3, 'o', color = 'darkgreen' , label = 'Indian client: English'     )
-# ~ plt.plot( lumpsum_regional*1e-3, 'o', color = 'r'         , label = 'Indian client: non-English' )
+# ~ plt.plot( lumpsum_foreign *1e-3, 'o', color = 'dodgerblue', label = 'Foreign client, English'    )
+# ~ plt.plot( lumpsum_Indian  *1e-3, 'o', color = 'darkgreen' , label = 'Indian client, English'     )
+# ~ plt.plot( lumpsum_regional*1e-3, 'o', color = 'r'         , label = 'Indian client, non-English' )
 # ~ plt.legend( loc = 'upper center' )
 # ~ plt.grid()
 # ~ plt.tight_layout()
@@ -267,40 +274,43 @@ def select_nonblanks( array1, array2 ):
 
 
 
-## Selects the non-blank pay_per_word data and the corresponding News flags.
-X5, News_flag_avPay = select_nonblanks( average_pay, News_flag )
+# ~ ## Selects the non-blank pay_per_word data and the corresponding News flags.
+# ~ X5, News_flag_pwPay = select_nonblanks( perword_pay, News_flag )
 
-## Selects the pay_per_word data only for News.
-inds5 = np.where( News_flag_avPay == 'Yes' )[0]
-avPay_News  = X5[inds5]
+# ~ ## Selects the pay_per_word data only for News.
+# ~ inds5 = np.where( News_flag_pwPay == 'Yes' )[0]
+# ~ pwPay_News  = X5[inds5]
 
-## Selects the non-blank remuneration data and the corresponding News flags.
-X6, News_flag_lumpsum = select_nonblanks( remuneration, News_flag )
+# ~ ## Selects the non-blank remuneration data and the corresponding News flags.
+# ~ X6, News_flag_lumpsum = select_nonblanks( remuneration, News_flag )
 
-## Selects the remuneration data only for News.
-inds6 = np.where( News_flag_lumpsum == 'Yes' )[0]
-lumpsum_News  = X6[inds6]
+# ~ ## Selects the remuneration data only for News.
+# ~ inds6 = np.where( News_flag_lumpsum == 'Yes' )[0]
+# ~ lumpsum_News  = X6[inds6]
 
-## Tests authenticity and checks outliers.
-print ('\n')
-print ( len(avPay_News), '\n' )
-print ( (avPay_News < 0).any(), '\n' )
-print ( avPay_News, '\n' )
-print ( len(lumpsum_News), '\n' )
-print ( (lumpsum_News < 0).any(), '\n' )
-print ( np.sort(lumpsum_News), '\n' )
-print ( 'Min   : ', np.min(   avPay_News) )
-print ( 'Max   : ', np.max(   avPay_News) )
-print ( 'Mean  : ', np.mean(  avPay_News) )
-print ( 'Median: ', np.median(avPay_News) )
-print ('\n')
+# ~ ## Tests authenticity and checks outliers.
+# ~ print ('\n')
+# ~ print ( len(pwPay_News), '\n' )
+# ~ print ( (pwPay_News < 0).any(), '\n' )
+# ~ print ( pwPay_News, '\n' )
+# ~ print ( len(lumpsum_News), '\n' )
+# ~ print ( (lumpsum_News < 0).any(), '\n' )
+# ~ print ( np.sort(lumpsum_News), '\n' )
+# ~ print ( 'Min   : ', np.min(   pwPay_News) )
+# ~ print ( 'Max   : ', np.max(   pwPay_News) )
+# ~ print ( 'Mean  : ', np.mean(  pwPay_News) )
+# ~ print ( 'Median: ', np.median(pwPay_News) )
+# ~ print ('\n')
 
 # ~ ## Plots histogram of news reports' pay per word.
-# ~ y_ticks = np.arange( 0, 10, 1 )
-# ~ plt.ylim( 0, 10 )
+# ~ x_ticks = np.arange( 0, 13, 1 )
+# ~ y_ticks = np.arange( 0, 11, 1 )
+# ~ plt.xlim( -0.5, 12 )
+# ~ plt.ylim(    0, 10 )
+# ~ plt.xticks( x_ticks, x_ticks )
 # ~ plt.yticks( y_ticks, y_ticks )
-# ~ plt.hist( avPay_News, rwidth = 0.7, color = 'r', alpha = 0.6, align = 'left' )
-# ~ plt.text( 10.5, 8.5, 'News' )
+# ~ plt.hist( pwPay_News, rwidth = 0.7, color = 'r', alpha = 0.6, align = 'left' )
+# ~ plt.text( 10.1, 8.5, 'News' )
 # ~ plt.xlabel( "Freelance news writers' pay per word [Indian Rupees]", labelpad=7 )
 # ~ plt.ylabel( "Number of instances" )
 # ~ plt.grid()
@@ -310,16 +320,19 @@ print ('\n')
 # ~ plt.close()
 
 # ~ ## Plots histogram of news reports' lump sum pay.
+# ~ x_ticks = np.arange( 0, 16, 1 )
 # ~ y_ticks = np.arange( 0, 15, 1 )
+# ~ plt.xlim( -1, 16 )
+# ~ plt.ylim(  0, 15 )
+# ~ plt.xticks( x_ticks, x_ticks )
 # ~ plt.yticks( y_ticks, y_ticks )
 # ~ plt.hist( lumpsum_News *1e-3, range = (0, 17), rwidth = 0.7, color = 'r', alpha = 0.6, align = 'left' )
-# ~ plt.text( 13, 13.2, 'News' )
+# ~ plt.text( 12.0, 13.3, 'News' )
 # ~ plt.xlabel( "Freelance news writers' lump sum remuneration [in 1000 Indian Rupees]", labelpad=7 )
 # ~ plt.ylabel( "Number of instances" )
-# ~ plt.text( 10.2, 7.35, "Ignores outlier (INR): 40K.", color='navy', bbox=dict(boxstyle='round', facecolor='wheat', edgecolor='y', alpha=0.7) )
+# ~ plt.text( 9.0, 7.35, "Ignores outlier (INR): 40.0 K.", color='navy', bbox=dict(boxstyle='round', facecolor='wheat', edgecolor='y', alpha=0.7) )
 # ~ plt.grid()
 # ~ plt.tight_layout()
 # ~ plt.savefig( './../plots/freelance_news_writerS_lump_sum--histogram.pdf' )
 # ~ plt.clf()
 # ~ plt.close()
-
